7284842c1667a307abbab95d28f345bdba672da5
David Arrizon <arrizon.david@gmail.com>
2019-10-02 10:07:21 -0500

    outlined body in html

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
-class Tomagotchi {
-	constructor() {
-	this.name
+class Tamagotchi {
+	constructor(name, age, hunger, sleepiness, boredom ) {
+	this.name = name;
+	this.age = age;
+	this.hunger = hunger;
+	this.sleepiness = sleepiness;
+	this.boredom = boredom;
 
 	}
 }
 
 const game = {
 	time: 0,
+	feedPet: function (){
+
+	},
+	changeLights: function (){
+
+	},
+	play: function (){
+
+	},
+
 }
