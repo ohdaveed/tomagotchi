f5c77cb9debe0851143182d2409557585b1aa67f
David Arrizon <arrizon.david@gmail.com>
2019-10-03 11:53:58 -0500

    fixed obj stuff

--- js/app.js
+++ js/app.js
@@ -5,17 +5,74 @@ class Tamagotchi {
 	this.hunger = hunger;
 	this.sleepiness = sleepiness;
 	this.boredom = boredom;
-
 	}
+};
+
+const game = {
+	time: 0,
+	inputArr: [],
+	pet: null,
+
+
+	newPet: function (name) {
+		const newTom = new Tamagotchi(name, 0, 0, 0, 0);
+		console.log(newTom)
+		this.pet = newTom
+		this.start()
+	},
+
+	start: function () {
+
+		const $timer = $('#timer');
+        const interval = setInterval(() => {
+	    	this.time++;
+	        if(this.time % 10 === 0){
+	        	this.increaseHunger()
+	        	this.increaseBoredom()
+	        	this.increaseAge()
+	       		// 	
+	        }
+	        // 
+	        $timer.text(`timer: ${this.time}s`)
+	    }, 1000)
+	},
+	feedPet: function (){
+		this.newTom['hunger'] -= 1;
+	},
+	changeLights: function (){
+		$('body').css('background-color', 'yellow')
+	},
+	play: function (){
+		this.newTom['boredom'] -= 1;
+	},
+
+	gameOver: function () {
+		if (this.pet['hunger'] === 10 || this.pet['sleepiness'] === 10 || this.pet['boredom'] === 10)
+			console.log("Game Over")
+	},
+	printStats: function () {
+
+  	},
+  	increaseAge: function(){
+  		this.pet['age'] += 1;
+  	},
+  	increaseHunger: function (){
+  		this.pet['hunger'] += 2;
+  	},
+  	increaseBoredom: function (){
+  		this.pet['boredom'] += 2;
+  	}
 }
+
+
 
 $('#submit-btn').on('click', () => {
   console.log('clicked');
   console.log( $('#input-box').val());
   const input = $('#input-box').val();
+  // game.inputArr.push(input)
+  game.newPet(input);
   event.preventDefault();
-  game.inputArr.push( $('#input-box').val())
-  game.start(userInput)
 
 });
 
@@ -36,54 +93,3 @@ $('#middle-button').on('click', () => {
   game.play();
   event.preventDefault();
 });
-
-const game = {
-	time: 0,
-	inputArr: [],
-	hunger: 0,
-	newPet: function (){
-	const newTom = new Tamagotchi(($('#input-box').val()), 0, 0, 0);
-	console.log('newPet')
-	},
-	start: function (name) {
-
-		const $timer = $('#timer');
-        const interval = setInterval(() =>{
-	    	this.time ++;
-	        if(this.time % 100 === 0){
-	        	this.increaseHunger()
-	        	this.increaseBoredom()
-	        	this.increaseAge()
-	        } else {
-	        	this.time++
-	        }
-	        $timer.text(`timer: ${this.time}s`)
-	        }, 1000)
-	},
-	feedPet: function (){
-		this.pet['hunger'] -= 1;
-	},
-	changeLights: function (){
-		$('body').css('background-color', 'yellow')
-	},
-	play: function (){
-		this.pet['boredom'] -= 1;
-	},
-
-	gameOver: function () {
-		if (hunger === 10 || sleepiness === 10 || boredom === 10)
-			console.log("Game Over")
-	},
-	printStats: function () {
-
-  	},
-  	increaseAge: function(){
-  		this.pet['age'] += 1;
-  	},
-  	increaseHunger: function (){
-  		this.pet['hunger'] += 2;
-  	},
-  	increaseBoredom: function (){
-  		this.pet['boredom'] += 2;
-  	}
-}
